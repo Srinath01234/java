243af0881613f00ce0b45c803ef98f23f8f0d194
Srinath01234 <srinathramani6@gmail.com>
2023-07-28 00:54:00 +0530

    Initial commit

---
+++ src/firstpackage/one.java
@@ -0,0 +1,33 @@
+package firstpackage;
+
+public class one {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("B.No:CTR246/551023");
+		System.out.println("Pay Mode:Cash     \s\s\s\s\s\s\s\s\s\s          Date:27/7/2023");
+		System.out.println("Snan:SOMANNA KARIGAR H   \s\s\s\s\s\s\s\s\s     Bill By:Raja.v");
+		System.out.println("\s\s\s\s\s\s\s\s\s\s\s\s\s\s\s\s\s\s\s\s\s\s\s\sPARCEL SERVICE");
+		System.out.println("----------------------------------------------------------");
+		System.out.println("Particulars           GST  HSH/SAC   QTY   Rate   Amount");
+		System.out.println("CARRY BAG MEDIUM      5%   996331   1.00   5.00   5.00");
+		System.out.println("PLAIN DOSAI           5%   996331   1.00   60.00  60.00");
+		System.out.println("PARATHA KURUMA        5%   996331   1.00   50.00  50.00");
+		System.out.println("SAMBAR VADAI(1 pc)    5%   996331   1.00   38.00  38.00");
+		System.out.println("----------------------------------------------------------");
+		System.out.println("Iot Itns4            Sub Total             150.00");
+		System.out.println("                     SGST 2.5%              3.8");
+		System.out.println("                     CGST 2.5%              3.0");
+		System.out.println("                     Round off              0");
+		System.out.println("                     -------------------------------------");
+		System.out.println("                     Total Amount           161.00");
+		System.out.println("\s\s\s\s\s\s\s\s\s\s\s\s\s\s       PLUS TIPS 10rs");
+		System.out.println("                     BILL AMOUNT 171.00/- \n"); 
+		System.out.println("Terminal No:  SS-156280");
+
+		
+	}
+
+}
+   
